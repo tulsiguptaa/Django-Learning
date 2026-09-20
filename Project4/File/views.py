af482713a61f66b80ad97708e7790fd9315811c8
tulsi <ttulsigupta836@gmail.com>
2026-09-20 23:54:53 +0530

    ss

--- Project4/File/views.py
+++ Project4/File/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import Form 
 from .models import Student,Post
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 # Create your views here.
 def upload_profile(request):
@@ -22,5 +23,8 @@ def view_profile(request):
     return render(request, 'accounts/view_profile.html', {'profiles': profiles})
 
 def post(request):
-    posts = Post.objects.all()
-    return render(request, 'accounts/post.html', {'posts': posts})
+    posts = Post.objects.all().order_by('id')
+    paginator = Paginator(posts, 1)
+    page_num = request.GET.get('page')
+    page_obj = paginator.get_page(page_num)
+    return render(request, 'accounts/post.html', {'page_obj' : page_obj})
