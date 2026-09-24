fa2add3640ee153e3231240a9916b3573ff2a02b
tulsi <ttulsigupta836@gmail.com>
2026-09-25 03:47:23 +0530

    dsd

--- Project5/blog/views.py
+++ Project5/blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
 
-# Create your views here.
+class PostList(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+class PostCreate(CreateView):
+    model = Post
+    template_name = 'blog/post_new.html'
+    fields = ['title', 'content']
+
+class PostUpdate(UpdateView):
+    model = Post
+    template_name = 'blog/post_edit.html'
+    fields = ['title', 'content']
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('post_list')
